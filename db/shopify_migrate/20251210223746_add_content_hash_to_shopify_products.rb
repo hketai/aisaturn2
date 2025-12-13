8853ee41f58028c03f454ea4f0950c5ed1ae94b4
Harun Ketenci <harun@Harun-MacBook-Air.local>
2025-12-13 06:38:11 +0300

    fix: Make migration idempotent for content_hash column

--- db/shopify_migrate/20251210223746_add_content_hash_to_shopify_products.rb
+++ db/shopify_migrate/20251210223746_add_content_hash_to_shopify_products.rb
@@ -1,8 +1,8 @@
 class AddContentHashToShopifyProducts < ActiveRecord::Migration[7.0]
   def change
-    add_column :shopify_products, :content_hash, :string
-    add_column :shopify_products, :image_hash, :string
-    add_index :shopify_products, :content_hash
+    add_column :shopify_products, :content_hash, :string unless column_exists?(:shopify_products, :content_hash)
+    add_column :shopify_products, :image_hash, :string unless column_exists?(:shopify_products, :image_hash)
+    add_index :shopify_products, :content_hash unless index_exists?(:shopify_products, :content_hash)
   end
 end
 
